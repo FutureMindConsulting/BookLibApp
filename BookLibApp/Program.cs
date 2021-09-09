fae5399d14ec4e5b40c19c54fac9989740ea5316
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2021-09-09 14:39:21 +0200

    Add project files.

---
+++ BookLibApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BookLibApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Book book2 = new Book();
+        }
+    }
+}
